090928041ef98fc12991ca18d698a1815021e3fe
HashenWatawala <hashanewatawala@gmail.com>
2025-10-20 14:35:53 +0530

    Refactor styles and layout for SelectWork component; remove unused CSS and enhance button accessibility

--- client/src/pages/Admin/SelectWork.jsx
+++ client/src/pages/Admin/SelectWork.jsx
@@ -4,27 +4,32 @@ import "../../styles/adminPage.css";
 
 const SelectWork = () => {
   return (
-    <div>
-          <div className="container-fluid">
-        <div className="row">
+    <div className="full-bg">
+      <div className="container-fluid">
+        <div className="row align-items-center">
           <div className="col-2" id="Emblem"></div>
           <div className="col-10" id="heading">
             <h1 className="heading1 text-white">GRAMA NILADHARI E-CERTIFICATION</h1>
           </div>
         </div>
-    </div>
-    <div className="NewBody">
-        <h1 className="text-white heading2 text-center">Select Corresponding Work</h1>
+      </div>
+
+      {/* Centered section */}
+      <div className="center-box">
+        <h1 className="text-white heading2 text-center mb-4">Select Corresponding Work</h1>
+
         <div className="loginbox text-white text-center">
-            <button type="button" className="btn btn-dark my-3"><Link to="/admin/DataEnter" >Register Person</Link></button>
-            <button type="button" className="btn btn-dark my-3"><Link to="/admin/InstituteReg" >Register Institute</Link></button>
-            <button type="button" className="btn btn-dark my-3"><Link to="/admin/CitizenRequest" >Personal Request</Link></button>
-            <button type="button" className="btn btn-dark my-3"><Link to="/admin/InstituteRequest" >Institute Request</Link></button>
-            <button type="button" className="btn btn-dark my-3"><Link to="/admin" onClick={() => localStorage.removeItem('admin')}>Log out</Link></button>
+          <Link to="/admin/DataEnter" className="btn btn-dark my-2 w-100">Register Person</Link>
+          <Link to="/admin/InstituteReg" className="btn btn-dark my-2 w-100">Register Institute</Link>
+          <Link to="/admin/CitizenRequest" className="btn btn-dark my-2 w-100">Personal Request</Link>
+          <Link to="/admin/InstituteRequest" className="btn btn-dark my-2 w-100">Institute Request</Link>
+          <Link to="/admin" className="btn btn-dark my-2 w-100" onClick={() => localStorage.removeItem('admin')}>
+            Log out
+          </Link>
         </div>
-    </div>
+      </div>
     </div>
   )
 }
 
-export default SelectWork
+export default SelectWork;
