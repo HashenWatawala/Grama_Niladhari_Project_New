683378d0eef31c4fdaa1eed91b10cfad30ef39d1
HashenWatawala <hashanewatawala@gmail.com>
2025-10-25 00:51:37 +0530

    Add AdminSignIn component and update routing; modify AdminLogin button text and enhance admin registration logic

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const AdminSchema = new mongoose.Schema({
   Registration_Number: { type: String, required: true },
   Password: { type: String, required: true },
-  gDivision: { type: String }
+  gDivision: { type: String },
+  signature: { type: String }
 });
 
 // If you want collection name exactly "Admin"
